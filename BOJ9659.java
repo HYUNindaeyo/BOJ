3d640265552965b79569734cd614c59da2d39332
HYUNindaeyo <43405705+HYUNindaeyo@users.noreply.github.com>
2021-01-17 20:05:01 +0900

    문제 이해 - BOJ9659

---
+++ BOJ9659.java
@@ -0,0 +1,16 @@
+package 백준공부;
+
+import java.util.Scanner;
+
+public class BOJ9659 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		long N = Long.parseLong(scan.next()); 
+		if (N%2==0) System.out.println("CY");
+		else System.out.println("SK");
+	}
+
+}
+
